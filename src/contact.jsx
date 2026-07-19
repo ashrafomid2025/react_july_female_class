ed89b78ffb4d1f5f27ee3c27e2a785783652140a
ashrafomid2025 <omidmashraf@gmail.com>
2026-07-19 20:03:12 +0430

    not found page added

---
+++ src/contact.jsx
@@ -0,0 +1,10 @@
+import Navbar from "./Navbar";
+
+export default function ContactPage() {
+  return (
+    <div>
+      <Navbar />
+      <h1 className="text-5xl"> this is contact us page</h1>
+    </div>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,34 @@
+<<<<<<< Updated upstream
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+=======
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AboutPage from "./about.jsx";
+import ServicesPage from "./Services.jsx";
+import ContactPage from "./contact.jsx";
+import NotfoundPage from "./NotFound.jsx";
+>>>>>>> Stashed changes
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+<<<<<<< Updated upstream
     <App />
+=======
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App />} path="/" />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/services" element={<ServicesPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/*" element={<NotfoundPage />} />
+      </Routes>
+    </BrowserRouter>
+>>>>>>> Stashed changes
   </StrictMode>,
 )
